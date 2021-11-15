a7f092500916598e4a0fd1c8b0c6ef2b838e7732
EmilyKral <emilykral@gmail.com>
2021-11-15 14:35:29 +0000

    cleaning up comments on client index.js

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -2,26 +2,8 @@
 
 const inputForm = document.querySelector("#inputform");
 const selectForm = document.querySelector("#showform");
-// const foodsList = document.querySelector("#foodsList");
-// const foodsNames = foodsList.children;
-// let foodsNamesList = document.querySelectorAll("#foodsList option");
-
-// function addClicks() {
-// 	let foodsNamesList = document.querySelectorAll("#foodsList option");
-// 	// console.log(foodsNamesList);
-// 	for (let i = 0; i < foodsNamesList.length; i++) {
-// 		// console.log(foodsNamesList[i]);
-// 		foodsNamesList[i].addEventListener("change", retrieveFood);
-// 	}
-// }
 
 inputForm.addEventListener("submit", addFood);
 selectForm.addEventListener("submit", retrieveFood);
 
-// function runLists() {
-// 	getFood();
-// 	setTimeout(addClicks, 1000);
-// }
-
 getFood();
-// runLists();
